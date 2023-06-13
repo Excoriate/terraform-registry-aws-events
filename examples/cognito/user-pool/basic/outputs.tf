741ad7a0f8814c9e6eb6b6f658835f085e454e00
Alex ATorres <atorres.ruiz@hotmail.com>
2023-06-13 05:19:17 +0200

    docs: add missing outputs and updated documentation

--- examples/cognito/user-pool/basic/outputs.tf
+++ examples/cognito/user-pool/basic/outputs.tf
@@ -13,3 +13,73 @@ output "user_pool_configuration" {
   value       = module.main_module.user_pool_configuration
   description = "The user pool configuration"
 }
+
+output "user_pool_id" {
+  value       =  module.main_module.user_pool_id
+  description = "The user pool id"
+}
+
+output "user_pool_arn" {
+  value       =  module.main_module.user_pool_arn
+  description = "The user pool arn"
+}
+
+output "user_pool_domain" {
+  value       =  module.main_module.user_pool_domain
+  description = "The user pool domain"
+}
+
+output "user_pool_schema" {
+  value       =  module.main_module.user_pool_schema
+  description = "The user pool schema"
+}
+
+output "user_pool_sms_configuration" {
+  value       =  module.main_module.user_pool_sms_configuration
+  description = "The user pool sms configuration"
+}
+
+output "user_pool_mfa_configuration" {
+  value       =  module.main_module.user_pool_mfa_configuration
+  description = "The user pool mfa configuration"
+}
+
+output "user_pool_lambda_config" {
+  value       =  module.main_module.user_pool_lambda_config
+  description = "The user pool lambda config"
+}
+
+output "user_pool_admin_create_user_config" {
+  value       =  module.main_module.user_pool_admin_create_user_config
+  description = "The user pool admin create user config"
+}
+
+output "user_pool_verification_message_template" {
+  value       =  module.main_module.user_pool_verification_message_template
+  description = "The user pool verification message template"
+}
+
+output "user_pool_email_configuration" {
+  value       =  module.main_module.user_pool_email_configuration
+  description = "The user pool email configuration"
+}
+
+output "user_pool_password_policy" {
+  value       =  module.main_module.user_pool_password_policy
+  description = "The user pool password policy"
+}
+
+output "user_pool_username_configuration" {
+  value       =  module.main_module.user_pool_username_configuration
+  description = "The user pool username configuration"
+}
+
+output "user_pool_account_recovery_setting" {
+  value       =  module.main_module.user_pool_account_recovery_setting
+  description = "The user pool account recovery setting"
+}
+
+output "user_pool_user_pool_add_ons" {
+  value       =  module.main_module.user_pool_user_pool_add_ons
+  description = "The user pool user pool add ons"
+}
--- modules/cognito/user-pool/outputs.tf
+++ modules/cognito/user-pool/outputs.tf
@@ -22,3 +22,73 @@ output "user_pool_configuration" {
   value       = local.user_pool_config
   description = "The user pool configuration"
 }
+
+output "user_pool_id" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.id][0]
+  description = "The user pool id"
+}
+
+output "user_pool_arn" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.arn][0]
+  description = "The user pool arn"
+}
+
+output "user_pool_domain" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.domain][0]
+  description = "The user pool domain"
+}
+
+output "user_pool_schema" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.schema][0]
+  description = "The user pool schema"
+}
+
+output "user_pool_sms_configuration" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.sms_configuration][0]
+  description = "The user pool sms configuration"
+}
+
+output "user_pool_mfa_configuration" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.mfa_configuration][0]
+  description = "The user pool mfa configuration"
+}
+
+output "user_pool_lambda_config" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.lambda_config][0]
+  description = "The user pool lambda config"
+}
+
+output "user_pool_admin_create_user_config" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.admin_create_user_config][0]
+  description = "The user pool admin create user config"
+}
+
+output "user_pool_verification_message_template" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.verification_message_template][0]
+  description = "The user pool verification message template"
+}
+
+output "user_pool_email_configuration" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.email_configuration][0]
+  description = "The user pool email configuration"
+}
+
+output "user_pool_password_policy" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.password_policy][0]
+  description = "The user pool password policy"
+}
+
+output "user_pool_username_configuration" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.username_configuration][0]
+  description = "The user pool username configuration"
+}
+
+output "user_pool_account_recovery_setting" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.account_recovery_setting][0]
+  description = "The user pool account recovery setting"
+}
+
+output "user_pool_user_pool_add_ons" {
+  value       = [for user_pool in aws_cognito_user_pool.this : user_pool.user_pool_add_ons][0]
+  description = "The user pool user pool add ons"
+}
