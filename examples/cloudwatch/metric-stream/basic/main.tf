236bb07b24409f72e099a6abb073a48a771057fb
Alex Torres R <atorres.ruiz@hotmail.com>
2024-05-23 06:00:50 +0200

    docs/update-cloudwatch-metric-stream-docs (#33)
    * refactor: Update AWS provider version in cloudwatch metric stream module configurations
    
    Bumped the AWS provider version to 5.41.0 in the cloudwatch metric stream module for compatibility. Added support for additional statistics in the main module configurations.
    
    * feat: Update main.tf to include new statistics configurations and filters

--- examples/cloudwatch/metric-stream/basic/main.tf
+++ examples/cloudwatch/metric-stream/basic/main.tf
@@ -1,6 +1,9 @@
 module "main_module" {
-  source     = "../../../../modules/cloudwatch/metric-stream"
-  is_enabled = var.is_enabled
-  tags       = var.tags
-  stream     = var.stream
+  source                    = "../../../../modules/cloudwatch/metric-stream"
+  is_enabled                = var.is_enabled
+  tags                      = var.tags
+  stream                    = var.stream
+  statistics_configurations = var.statistics_configurations
+  include_filters           = var.include_filters
+  exclude_filters           = var.exclude_filters
 }
